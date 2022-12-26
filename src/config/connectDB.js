0e1abc344422d75890fc45b2c2f5f53cbdb61338
Thanh Toan <thanhtoan84200226@gmail.com>
2022-12-27 01:18:52 +0700

    connectDB SQL

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,8 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('NodeJSBackend', 'root', null, {
-  host: 'localhost',
-  dialect: 'mysql'
+const sequelize = new Sequelize('NodeJSBackend', 'sa', '123456789', {
+    dialect: 'mssql',
+    host: 'localhost',
+    port: 1433, // Default port
+    dialectOptions: {
+        instanceName: 'SQLEXPRESS',
+        requestTimeout: 30000
+    },
+    pool: {
+        max: 50,
+        min: 0,
+        idle: 10000
+    }
 });
 
 let connectDB = async() => {
